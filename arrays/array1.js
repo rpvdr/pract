363427d7ae8b8287c84e381464c6404e6875b3ed
rpvdr <daria.ropaieva@proton.me>
2024-10-29 14:26:19 +0100

    arrays

---
+++ arrays/array1.js
@@ -0,0 +1,35 @@
+// arr1 = [1,-4,7,12]
+// function positiveSum(arr) {
+//     const positiveNumbers = arr.filter(el => el > 0)
+//     let sum = 0
+//     positiveNumbers.map(el => sum += el)
+//     return sum
+//   }
+// console.log(positiveSum(arr1))
+
+// arr2 = [1, 2, 3]
+// function maps(x){
+//     return x.map(el => el*2)
+// }
+// console.log(maps(arr2))
+
+// arr3 = [1, 2, 3, 4]
+// function grow(x){
+//     product = 1;
+//     x.map(el => product *= el)
+//     return product
+// }
+// console.log(grow(arr3))
+
+// with reduce
+// arr3 = [1, 2, 3, 4]
+// function grow(x){
+//     const initialValue = 1;
+//     const sumWithInitial = x.reduce(
+//       (accumulator, currentValue) => accumulator * currentValue,
+//       initialValue,
+//     ); 
+//     return sumWithInitial
+// }
+// console.log(grow(arr3))
+
