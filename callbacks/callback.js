41f1531b7e91dee5df6412f830affef74cbe3590
rpvdr <daria.ropaieva@proton.me>
2024-11-23 23:32:17 +0100

    callbacks easy

---
+++ callbacks/callback.js
@@ -0,0 +1,16 @@
+// function greet(name, callback) {
+//     console.log(`Привет, ${name}!`);
+//     callback();
+// }
+
+// function sayGoodbye() {
+//     console.log('До встречи!');
+// }
+
+// greet('Дарья', sayGoodbye);
+
+//we need a callback function to pass a function as an argument, not the result of the function undefined
+// setTimeout(function(){console.log("Hello")},2000);
+
+// let interval = setInterval(()=>{console.log("Hello!")}, 3000); 
+// setTimeout(()=>{clearInterval(interval);},7000)
